0233520e3b4338693760ef12541a2f1731396b21
Vitor Boccio <vitorboccio@gmail.com>
2025-09-24 15:32:58 -0600

    react sdk env

--- src/config.ts
+++ src/config.ts
@@ -1,29 +1,10 @@
 const ENVIRONMENTS = {
-  development: {
-    API_URL: "http://localhost:5000",
-    WS_URL: "ws://localhost:5000",
-  },
-  staging: {
-    API_URL: "https://api.staging.useflagpole.dev",
-    WS_URL: "wss://api.staging.useflagpole.dev",
-  },
-  production: {
-    API_URL: "https://useflagpole-api.onrender.com",
-    WS_URL: "wss://useflagpole-api.onrender.com",
-  },
+  API_URL: "https://useflagpole-api.onrender.com",
+  WS_URL: "wss://useflagpole-api.onrender.com",
 } as const;
 
-const getEnvironment = () => {
-  if (typeof window !== "undefined") {
-    const hostname = window.location.hostname;
-    if (hostname === "useflagpole.dev") return "production";
-    if (hostname === "staging.useflagpole.dev") return "staging";
-  }
-  return "development";
-};
-
 export const DEFAULT_CONFIG = {
-  ...ENVIRONMENTS[getEnvironment()],
+  ...ENVIRONMENTS,
   VERSION: "0.0.1",
   SOCKET_TIMEOUT: 20000,
   RECONNECTION_ATTEMPTS: 5,
